eafbb5ca67fcbd57f5c6883e3aa95fa6d5365279
davidjacob94 <davidjacob94@gmail.com>
2015-12-09 23:25:52 -0500

    Plotted Project 3 Part 1 CLT vs Precise

--- part1.m
+++ part1.m
@@ -8,7 +8,7 @@ clear all
 clc
 
 n = 100000;
-interval = [0:0.001:1]
+interval = [0:0.001:1];
 
 %Creates n samples of 2, 4, 16, 100 columns long uniformly distributed
 rM2 = rand(n, 2);
@@ -18,8 +18,8 @@ rM100 = rand(n, 100);
 
 
 %Calculates mean along Columns
-mM2 = mean(rM2, 2)
-mM4 = mean(rM4, 2)
+mM2 = mean(rM2, 2);
+mM4 = mean(rM4, 2);
 mM16 = mean(rM16, 2);
 mM100 = mean(rM100,2);
 
@@ -36,3 +36,44 @@ histm16 = histm16/n/.001;
 
 [histm100, z] = hist(mM100, interval);
 histm100 = histm100/n/.001;
+
+%General Sigma and for specific N values
+sigma = sqrt(1/12);
+sig2 = sigma/sqrt(2);
+sig4 = sigma/sqrt(4);
+sig16 = sigma/sqrt(16);
+sig100 = sigma/sqrt(100);
+
+%Generates approximate pdfs for Mn
+approx2 = normpdf(z, .5, sig2);
+approx4 = normpdf(z, .5, sig4);
+approx16 = normpdf(z, .5, sig16);
+approx100 = normpdf(z, .5, sig100);
+
+
+%Plotting
+figure(1)
+subplot(2,2,1)
+plot(z, histm2,'-b', z, approx2, 'r')
+legend('CLT', 'Precise')
+title('Precise vs Approximate (CLT) for n = 2')
+xlabel('Mean of X')
+ylabel('f(x)')
+
+subplot(2,2,2)
+plot(z, histm4,'-b', interval, approx4, 'r')
+title('Precise vs Approximate (CLT) for n = 4')
+xlabel('Mean of X')
+ylabel('f(x)')
+
+subplot(2,2,3)
+plot(z, histm16,'-b', z, approx16, 'r')
+title('Precise vs Approximate (CLT) for n = 16')
+xlabel('Mean of X')
+ylabel('f(x)')
+
+subplot(2,2,4)
+plot(z, histm100,'-b', z, approx100, 'r')
+title('Precise vs Approximate (CLT) for n = 100')
+xlabel('Mean of X')
+ylabel('f(x)')
